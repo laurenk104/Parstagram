e892f233e9c20acfed667d27595c97b097c12590
Lauren Krieger <laurenk104@fb.com>
2020-07-08 10:37:00 -0700

    posts recyclerview

--- app/src/main/java/com/example/parstagram/fragments/ComposeFragment.java
+++ app/src/main/java/com/example/parstagram/fragments/ComposeFragment.java
@@ -79,7 +79,6 @@ public class ComposeFragment extends Fragment {
             }
         });
 
-        //queryPosts();
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -165,23 +164,6 @@ public class ComposeFragment extends Fragment {
                 Toast.makeText(getContext(), "Post save was successful!", Toast.LENGTH_SHORT).show();
                 etDescription.setText("");
                 ivPostImage.setImageResource(0);
-            }
-        });
-    }
-
-    private void queryPosts() {
-        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
-        query.include(Post.KEY_USER);
-        query.findInBackground(new FindCallback<Post>() {
-            @Override
-            public void done(List<Post> posts, ParseException e) {
-                if (e != null) {
-                    Log.e(TAG, "Issue with getting posts", e);
-                    return;
-                }
-                for (Post post : posts) {
-                    Log.i(TAG, "Post: " + post.getDescription() + ", username: " + post.getUser().getUsername());
-                }
             }
         });
     }
